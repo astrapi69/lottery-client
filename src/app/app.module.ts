08a3479d61f891a34058e21317c6bc71ba401a08
Asterios Raptis <asterios.raptis@gmx.net>
2020-06-16 18:09:55 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import {APP_BASE_HREF} from '@angular/common';
+import {AppRoutingModule} from './app-routing.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    AppComponent,
+  ],
+  bootstrap:    [ AppComponent ],
+  providers: [{provide: APP_BASE_HREF, useValue : '/' }]
+})
+export class AppModule { }
